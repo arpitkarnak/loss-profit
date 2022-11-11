6ea18e7797ba0b2b77646a7930813e060b097bff
arpitkarnak <arpitkarnak@users.noreply.github.com>
2022-11-12 01:00:21 +0530

    Project Finished
    This project is finished completely

--- app.js
+++ app.js
@@ -1,42 +1,116 @@
 
-var initialPrice = document.querySelectorAll('#initial-price');
-var stocksQuantity = document.querySelectorAll('#stocks-quantity');
-var currentPrice = document.querySelectorAll('#current-price');
+var initialPrice = document.querySelector('#initial-price');
+var stocksQuantity = document.querySelector('#stocks-quantity');
+var currentPrice = document.querySelector('#current-price');
 var clickBtn = document.querySelector('#click-btn');
-var output1 = document.querySelector('#output')
+var output1 = document.querySelector('.output');
 
 
 
-function calculateProfitAndLoss(initial, quantity,current){
-    if(initial > current) {
-        var loss = (initial - current) * quantity;
-        var lossPercentage = (loss / initial) * 100;
+clickBtn.addEventListener('click',submitHandler)
 
-        console.log(`Hey the loss is ${loss} and the percentage is ${lossPercentage}%` )
+
+function submitHandler(){
+
+    
+ 
+    var intPrice = Number(initialPrice.value);
+    var quant = Number(stocksQuantity.value)
+   
+    var currPrice = Number(currentPrice.value);
+
+    calculateProfitAndLoss(intPrice,quant,currPrice)
+}
+
+
+
+function financial(x) {
+    return Number.parseFloat(x).toFixed(2);
+  }
+
+
+
+
+
+function calculateProfitAndLoss(initial,quantity,current){
+
+    if(initial==''){
+
+        output1.style.display="block";
+        output1.style.color="white";
+        output1.style.background="rgba(132, 190, 230, 0.6)";
+
+        output1.innerText="Please Fill All the Value"
 
     }
-    else if(initial < current){
+    else if(current==''){
 
-        var profit = (current - initial)* quantity;
+        output1.style.display="block";
+        output1.style.color="white";
+        output1.style.background="rgba(132, 190, 230, 0.6)";
+
+        output1.innerText="Please Fill All the Value "
+
+    }
+    else if(quantity==''){
+
+        output1.style.display="block";
+        output1.style.color="white";
+        output1.style.background="rgba(132, 190, 230, 0.6)";
+
+        output1.innerText="Please Fill All the Value"
+
+    }
+    else if(initial > current) {
+        var loss = (initial - current) * quantity;
+        var lossPercentage = (loss / initial) * 100;
+        var fixLossPercentage = financial(lossPercentage);
+        output1.style.display="block";
+        output1.style.color="green";
+        output1.style.background="rgba(132, 190, 230, 0.6)";
+
+        output1.innerText =`Total Loss : '${loss}' & Loss Percentage: ${fixLossPercentage}%`
+        
+        // console.log(`Hey the loss is ${loss} and the percentage is ${lossPercentage}%` )
+
+    } 
+
+    else if(current > initial){
+
+        var profit = (current - initial) * quantity;
         var profitPercentage = (profit / initial)*100;
+        var fixProfitPercentage = financial(profitPercentage);
 
-        console.log(`Hey buddy! The Profit is ${profit} and the Percentage is ${profitPercentage}%` )
+        output1.style.display="block";
+        output1.style.color="red";
+        output1.style.background="rgba(132, 190, 230, 0.6)";
+
+
+        output1.innerText =`Total Profit: "${profit}" & Profit Percentage : ${fixProfitPercentage}%`
+
+
+        // console.log(`Hey buddy! The Profit is ${profit} and the Percentage is ${profitPercentage}%` )
 
         
     }
     else {
-        console.log(`No pain No Gain & No Gain No Pain`)
+
+        output1.style.display="block";
+        output1.style.color="white";
+        output1.style.background="rgba(132, 190, 230, 0.6)";
+
+        output1.innerText = `No pain No Gain & No Gain No Pain`
+        // console.log(`No pain No Gain & No Gain No Pain`)
     }
 
 }
 
 
-calculateProfitAndLoss(50,10,10);
-calculateProfitAndLoss(10,10,100);
-calculateProfitAndLoss(10,10,10);
+// calculateProfitAndLoss(50,10,10);
+// calculateProfitAndLoss(10,10,100);
+// calculateProfitAndLoss(10,10,10);
 
 
 
 
 
-clickBtn.addEventListener('click',calculateProfitAndLoss)
